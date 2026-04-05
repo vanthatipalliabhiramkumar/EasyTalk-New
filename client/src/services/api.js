6985a225d830a597657d9a016a12f22951948508
vanthatipalliabhiramkumar <vanthatipalliabhiramkumar@gmail.com>
2026-04-05 19:52:21 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-
+//import axios from 'axios';
+import api from '../utils/api'; // adjust path
 //const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 const API_URL = import.meta.env.VITE_API_URL;
 
