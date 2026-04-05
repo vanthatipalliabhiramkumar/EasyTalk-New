5c857b999fc07e8de2df899fadbf124c599f576d
vanthatipalliabhiramkumar <vanthatipalliabhiramkumar@gmail.com>
2026-04-05 19:32:15 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'whatwg-fetch';
+//import 'whatwg-fetch';
 if (typeof window !== 'undefined') {
   globalThis.Request = window.Request;
   globalThis.Response = window.Response;
