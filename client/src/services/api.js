c132a4f137abd6d0b9ed3725d943a3db5a7347a7
vanthatipalliabhiramkumar <vanthatipalliabhiramkumar@gmail.com>
2026-04-05 18:52:45 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
+if (!API_URL) {
+  console.error("❌ VITE_API_URL not set!");
+}
 const api = axios.create({
   baseURL: API_URL,
   headers: {
