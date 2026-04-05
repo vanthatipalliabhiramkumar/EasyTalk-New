f0ded46a784c5d5d9b1666aca85f128ca718d775
vanthatipalliabhiramkumar <vanthatipalliabhiramkumar@gmail.com>
2026-04-05 19:21:25 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import 'whatwg-fetch';
+if (typeof window !== 'undefined') {
+  globalThis.Request = window.Request;
+  globalThis.Response = window.Response;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
